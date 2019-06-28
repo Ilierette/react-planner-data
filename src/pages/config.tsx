3c46a380912d4d20bafaebbdd154c8cc42ff9c2a
Karolina Pietraś <karolina.pietra@loyaltypoint.pl>
2019-06-28 15:12:51 +0200

    yes

--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -1,7 +1,14 @@
 import * as firebase from 'firebase';
-import { keys } from '../../keys';
 
-const firebaseConfig = keys;
+const firebaseConfig = {
+    apiKey: "AIzaSyAYmtwSekzGtfGwWIOCXkimRzZL5eJIaGY",
+    authDomain: "game-tools-b5a6d.firebaseapp.com",
+    databaseURL: "https://game-tools-b5a6d.firebaseio.com",
+    projectId: "game-tools-b5a6d",
+    storageBucket: "game-tools-b5a6d.appspot.com",
+    messagingSenderId: "573871543925",
+    appId: "1:573871543925:web:e8037bddb28b819b"
+};
 
 firebase.initializeApp(firebaseConfig)
 
